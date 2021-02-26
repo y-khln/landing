75d2cb60a53ee096da5f7dcc9b1d7dd018d03a25
y-khln <79505730+y-khln@users.noreply.github.com>
2021-02-26 12:18:25 +0400

    Add files via upload

--- script (1).js
+++ script (1).js
@@ -2,10 +2,16 @@
 //1 и 1*: Вывод имени + сохранение
 function Name(){
     let userName = document.getElementById('name').value;
-    localStorage.setItem('user',userName)
-    document.getElementById("userName").innerHTML =localStorage.getItem('user');
-    document.getElementById("hello").innerHTML = "Здравствуйте, "+localStorage.getItem('user')+"!";
-    //localStorage.setItem(userName);
+    let nameId = document.getElementById('name');
+    if (userName == "") {
+        alert("Необходимо ввести имя");
+    }
+    if(!nameId.checkValidity()){document.getElementById("hello").innerHTML = "Введите корректные данные";}
+    else {
+        localStorage.setItem('user',userName)
+        document.getElementById("userName").innerHTML =localStorage.getItem('user');
+        document.getElementById("hello").innerHTML = "Здравствуйте, "+localStorage.getItem('user')+"!";
+    }
 }
 function LoadName(){
     if(localStorage.getItem('user')!=null) document.getElementById("userName").innerHTML =localStorage.getItem('user');
@@ -35,7 +41,13 @@ function Close6(){
 function Area(){
     let side=document.getElementById('q1_1').value;
     let height=document.getElementById('q1_2').value;
-    document.getElementById("area").innerHTML = "Площадь треугольника: "+(side*height/2);
+    if (side == ""||height=="") {
+        alert("Необходимо ввести все требуемые данные");
+    }
+    let sideId=document.getElementById('q1_1');
+    let heightId=document.getElementById('q1_2');
+    if(!sideId.checkValidity()||!heightId.checkValidity()){document.getElementById("area").innerHTML = "Введите корректные данные";}
+    else document.getElementById("area").innerHTML = "Площадь треугольника: "+(side*height/2);
 }
 function Punkt2(){
     document.getElementById('how_2').style.display="block";
@@ -44,8 +56,15 @@ function Punkt2(){
 function Compare(){
     let st1=document.getElementById('q2_1').value;
     let st2=document.getElementById('q2_2').value;
-    if(st1.length== st2.length) document.getElementById("comparator").innerHTML = "True";
-    else document.getElementById("comparator").innerHTML = "False";
+
+    if (st1 == ""||st2=="") {
+        alert("Необходимо ввести все требуемые данные");
+    }
+    else{
+        if(st1.length== st2.length) document.getElementById("comparator").innerHTML = "True";
+        else document.getElementById("comparator").innerHTML = "False";
+    }
+    
 }
 function Punkt3(){
     document.getElementById('how_3').style.display="block";
@@ -53,48 +72,63 @@ function Punkt3(){
 //4: Нахождение мин и макс в массиве из 5 элементов
 function MaxMin(){
     let m1=document.getElementById('q3').value.split(" ");
+    let m1Id=document.getElementById('q3');
     if(m1.length!=5) document.getElementById("maxi").innerHTML = "Нужно 5 элементов";
+    if(!m1Id.checkValidity()){
+        document.getElementById("maxi").innerHTML = "Введите корректные данные";
+        document.getElementById("mini").innerHTML = "";
+    }
     if(m1.length!=5) document.getElementById("mini").innerHTML = "";
-    else{
+    else if(m1Id.checkValidity()){
         let max=Math.max.apply(null,m1);
         let min=Math.min.apply(null,m1);
         document.getElementById("maxi").innerHTML = "Максимальный элемент: "+max;
         document.getElementById("mini").innerHTML = "Минимальный элемент: "+min;
     }
+        
 }
 function Punkt4(){
     document.getElementById('how_4').style.display="block";
 }
-//5: Таймер (секундомер)
-let count, minute = 0,second = 0,hour = 0;
+//5: Таймер 
+let score, minute = 0,second = 0,hour = 0;
 let Timer = {
   addTime() {
     document.getElementById("hour").innerHTML = hour;
-    document.getElementById("min").innerHTML = minute;
-    document.getElementById("sec").innerHTML = second;
+    document.getElementById("minu").innerHTML = minute;
+    document.getElementById("seco").innerHTML = second;
     ++second;
+    if(second<10){document.getElementById("seco2").style.display="block";}
+    if(second>10){document.getElementById("seco2").style.display="none";}
     if (second == 60){second = 0; minute++;}
+    if(minute>10){document.getElementById("minu2").style.display="none";}
+    if(minute<10){document.getElementById("minu2").style.display="block";}
+    if(hour>10){document.getElementById("hour2").style.display="none";}
+    if(hour<10){document.getElementById("hour2").style.display="block";}
     if (minute == 60){hour++; minute = 0;}
-    count = window.setTimeout("Timer.addTime()",1000);
+    score = window.setTimeout("Timer.addTime()",1000);
   },
   start() {
-    if (!count)
+    if (!score)
       this.addTime();
   },
   pause() {
-    if (count) {
-      clearTimeout(count);
-      count=0;}
+    if (score) {
+      clearTimeout(score);
+      score=0;}
   },
   reset(){
+    document.getElementById("seco2").style.display="block";
+    document.getElementById("minu2").style.display="block";
+    document.getElementById("hour2").style.display="block";
     hour = 0;
     minute = 0;
     second = 0;
-    window.clearInterval(count);
-    count = 0;
+    window.clearInterval(score);
+    score = 0;
     document.getElementById("hour").innerHTML="0";
-    document.getElementById("min").innerHTML="0";
-    document.getElementById("sec").innerHTML="0";
+    document.getElementById("minu").innerHTML="0";
+    document.getElementById("seco").innerHTML="0";
   },
 };
 function Punkt5(){
@@ -103,17 +137,16 @@ function Punkt5(){
 //6: Тест
 function Test(){
     let answers = document.querySelectorAll('.test_q'); //Все элементы div с вопросами
-    let score = 0; //Количество верных ответов
+    let score = 0,flag=0; //Количество верных ответов
     let rightAnswers = ["1_1", "2_2","3_2","4_2","5_3","6_3","7_2","8_1","9_1","10_3"] //Список верных ответов
     let falseAnswers=[];
     let j=0;
     for (let i = 0; i < 10; i++) {
         let chosenAnswer = answers[i].querySelector("input[type='radio']:checked"); //Из массива вопросов выбираем элемент радио, который выбрал пользователь
-        if (chosenAnswer==null) chosenAnswer="0"; //Засчитываем пропущенные вопросы как неправильные
-        //alert("Пожалуйста, ответьте на все вопросы"); //При пропуске вопросов просим ответить на все 
-        if (chosenAnswer && chosenAnswer.id == rightAnswers[i]) //Подсчет результата
+        if (chosenAnswer==null) alert("Пожалуйста, ответьте на все вопросы");
+        else if (chosenAnswer && chosenAnswer.id == rightAnswers[i]) //Подсчет результата
         {
-            score++; 
+            ++score; 
         }
         else{ //Подсчет в каких вопросах пользователь указал неправильные ответы
             falseAnswers[j]=i+1;
@@ -124,21 +157,26 @@ function Test(){
         console.log("Правильный ответ: " + rightAnswers[i]);
         console.log("Очки: " + score);
     }
-    document.getElementById("testResult").innerHTML="Ваш результат: "+score+"/10";
-    document.getElementById("testTrueFalse").innerHTML="Вы ответили неверно на вопросы: "+ falseAnswers;
+        document.getElementById("testResult").innerHTML="Ваш результат: "+score+"/10";
+        document.getElementById("testTrueFalse").innerHTML="Вы ответили неверно на вопросы: "+ falseAnswers;
 }
 function Punkt6(){
     document.getElementById('how_6').style.display="block";
 }
 //7: Заставка
 function Background(){
+    let userName = document.getElementById('name').value;
+    if (userName == ""||userName==null) {
+        alert("Необходимо ввести имя");
+    }
+    else{
     document.getElementById("zast_text").style.display="block";//Заставка появляется
     document.getElementById("zastavka").style.display="block";
     let date=new Date();
     let todayDate = date.getDate()+"."+date.getMonth()+"."+date.getFullYear();
     let us = "Пользователь: "+localStorage.getItem('user');
     document.getElementById("zast_text").innerHTML=todayDate+"<br>"+us+"</br>";
-    console.log(todayDate,us);
+    console.log(todayDate,us);}
 }
 function Back(){
     document.getElementById("zast_text").style.display="none";//Заставка пропадает
